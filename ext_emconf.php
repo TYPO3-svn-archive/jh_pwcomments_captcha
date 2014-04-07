169b74a7887bfddee2d8d5f66d70a9d1625091ab
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-04-07 14:11:08 +0000

    Fixer error in typo3 dependencies
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_pwcomments_captcha/trunk@84632 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'extbase' => '6.0',
 			'fluid' => '6.0',
-			'typo3' => '6.0-6.2.99',
+			'typo3' => '6.0.0-6.2.99',
 			'pw_comments' => '2.0.0',
 		),
 		'conflicts' => array(
